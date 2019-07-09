f5b1af05cd8fb8de2ebb9797c4d441c22efc4aac
taiyang1994 <1209450705@qq.com>
2019-07-09 17:23:43 +0800

    app.vue eslint 修正版

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div class="section">
       <div @click="showCashier">点我显示收银台</div>
     </div>
-  
     <div class="section">
       <div @click="isShowActionSheetIPhone8=true">IPhone8ActionSheet</div>
     </div>
@@ -42,7 +41,7 @@
         <tag text="我是内容一" />
         <tag text="我是文字内容" type="warn" style="margin-left: 10px;" />
       </div>
-      <br />
+      <br>
       <tag text="我是文字内容" flat />
     </div>
     <div class="section">
@@ -72,7 +71,7 @@
             width="100%"
             src="https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=b372bd3285d4b31cf03c93bdbfed4042/2cf5e0fe9925bc31a5ffa7e950df8db1cb137025.jpg"
             alt
-          />
+          >
           <div class="zm-dz">100000</div>
         </div>
       </md-landscape>
@@ -100,9 +99,9 @@
         <div slot="content">
           <div style="line-height: 26px;">
             预授予额度是针对优质客户专门匹配的信用贷款额度，您可通过手机银行直接在线办理、签约、提款。
-            <br />1、预授信额度在通知您之日起30天内有效，请及时办理审批及提款；
-            <br />2、预授信额度以实际审批为准；
-            <br />3、预授信额度期限最长1年。
+            <br>1、预授信额度在通知您之日起30天内有效，请及时办理审批及提款；
+            <br>2、预授信额度以实际审批为准；
+            <br>3、预授信额度期限最长1年。
           </div>
         </div>
       </md-landscape>
@@ -118,7 +117,9 @@
       <button
         style="margin-top: 10px;"
         @click="stepsHorn[2].SysNo = '0'; stepsHorn[4].SysNo = '1'"
-      >go step 5</button>
+      >
+go step 5
+</button>
     </div>
     <div class="section no-padding">
       <card-cell :list="cardList" />
@@ -138,77 +139,76 @@
     <div class="section no-padding">
       <card-cell :list="cardList" layout="mini" :is-switchable="false" :selected-index="4" />
     </div>
-   
     <div>
-      <Action-sheet
-       v-model="isShowActionSheetIPhone8"
-        :options="ActionSheet1"
-        is-iphone-x
-        @selected="handleSelect" 
-      ></Action-sheet>
+      <action-sheet
+      v-model="isShowActionSheetIPhone8"
+      :options="ActionSheet1"
+      is-iphone-x
+      @selected="handleSelect"
+      />
     </div>
     <div>
-      <Action-sheet
+      <action-sheet
         v-model="isShowActionSheetIPhone6"
         :options="ActionSheet1"
-        :defaultIndex="1"
+        :default-index="1"
         @selected="handleSelect"
-      ></Action-sheet>
+      />
     </div>
     <div>
-      <Action-sheet
+      <action-sheet
         v-model="isShowActionSheetExit"
         :title="ActionSheetExit.title"
         :options="ActionSheetExit.ActionSheet"
         @selected="handleSelect"
-      ></Action-sheet>
+      />
     </div>
     <div>
-      <Action-sheet
+      <action-sheet
         v-model="isShowActionSheetLineState"
         :title="ActionSheetLine.title"
         :options="ActionSheetLine.ActionSheet"
-        :defaultIndex="1"
-        @selected="handleSelect"   
-      ></Action-sheet>
+        :default-index="1"
+        @selected="handleSelect"
+      />
     </div>
     <div>
-      <Action-sheet
+      <action-sheet
         v-model="isShowActionSheetLine"
         :options="ActionSheetLine.ActionSheet2"
-        :defaultIndex="1"
+        :default-index="1"
         @selected="handleSelect"
-      ></Action-sheet>
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import timeline from "@mand-mobile/timeline";
-import CardCell from "./components/business/card-cell/card-cell";
-import Marquee from "./components/business/marquee";
-import Landscape from "@mand-mobile/landscape";
-import Button from "@mand-mobile/button";
-import Tag from "./components/business/tag";
-import picker from "./components/business/picker/picker";
-import cashier from "./components/business/cashier/index";
-import ActionSheet from "../src/components/mand-mobile/components/action-sheet";
+import Vue from 'vue';
+import timeline from '@mand-mobile/timeline';
+import CardCell from './components/business/card-cell/card-cell';
+import Marquee from './components/business/marquee';
+import Landscape from '@mand-mobile/landscape';
+import Button from '@mand-mobile/button';
+import Tag from './components/business/tag';
+import picker from './components/business/picker/picker';
+import cashier from './components/business/cashier/index';
+import actionSheet from '../src/components/mand-mobile/components/action-sheet';
 Vue.use(Landscape);
 Vue.use(Button);
-Vue.use(ActionSheet);
+Vue.use(actionSheet);
 export default {
-  name: "App",
+  name: 'App',
   components: {
     timeline,
     [CardCell.name]: CardCell,
     [Marquee.name]: Marquee,
     [Tag.name]: Tag,
-    ActionSheet,
+    actionSheet
   },
   data() {
     return {
-      list: "1234567".split(""),
+      list: '1234567'.split(''),
       picker: null,
       fullDatePicker: null,
       monthDatePicker: null,
@@ -226,52 +226,51 @@ export default {
       cashier: null,
       steps: [],
       stepsHorn: [
-        { timeFlag: "01.07", timeFlagMsg: "购买日期" },
-        { timeFlag: "02.07", timeFlagMsg: "确认中" },
-        { timeFlag: "03.17", timeFlagMsg: "起息日", SysNo: "1" },
-        { timeFlag: "06.12", timeFlagMsg: "下一起息日" },
-        { timeFlag: "06.12", timeFlagMsg: "下一到期日" }
+        {timeFlag: '01.07', timeFlagMsg: '购买日期'},
+        {timeFlag: '02.07', timeFlagMsg: '确认中'},
+        {timeFlag: '03.17', timeFlagMsg: '起息日', SysNo: '1'},
+        {timeFlag: '06.12', timeFlagMsg: '下一起息日'},
+        {timeFlag: '06.12', timeFlagMsg: '下一到期日'}
       ],
- 
       ActionSheet1: [
         {
-          label: "面容ID登录",
+          label: '面容ID登录',
           value: 0,
           type: ' primary'
         },
         {
-          label: "密码登录",
+          label: '密码登录',
           value: 1,
           type: ' primary'
         },
         {
-          label: "触控ID登录",
+          label: '触控ID登录',
           value: 2,
           type: 'primary'
         }
       ],
       ActionSheetExit: {
-        title: "退出后不会删除任何历史数据，下次登录依然可以使用本账号。",
+        title: '退出后不会删除任何历史数据，下次登录依然可以使用本账号。',
         ActionSheet: [
           {
-            label: "退出登录",
+            label: '退出登录',
             value: 0,
-            type:"warning"
+            type: 'warning'
           }
         ]
       },
       ActionSheetLine: {
-        title: "解除绑定后，手机银行将无法使用该卡片",
+        title: '解除绑定后，手机银行将无法使用该卡片',
         ActionSheet: [
           {
-            label: "解除绑定",
+            label: '解除绑定',
             value: 0,
-             type: 'primary'
+            type: 'primary'
           }
         ],
         ActionSheet2: [
           {
-            label: "变更资金库",
+            label: '变更资金库',
             value: 0,
             type: 'primary'
           }
@@ -279,70 +278,70 @@ export default {
       },
       cardList: [
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000668655",
-          E_ACTP_FLAG: "2",
-          E_ACCOUNT_BALANCE: "0.00",
-          E_AVAILABLEAMOUNT: "132,123,123.3456",
-          E_CARD_NO: "62176027080005011",
-          E_ISSUE_BRANCH: "0002",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "N"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000668655',
+          E_ACTP_FLAG: '2',
+          E_ACCOUNT_BALANCE: '0.00',
+          E_AVAILABLEAMOUNT: '132,123,123.3456',
+          E_CARD_NO: '62176027080005011',
+          E_ISSUE_BRANCH: '0002',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'N'
         },
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000663030",
-          E_ACTP_FLAG: "3",
-          E_ACCOUNT_BALANCE: "0.00",
-          E_AVAILABLEAMOUNT: "0.00",
-          E_CARD_NO: "62176022460000054",
-          E_ISSUE_BRANCH: "0101",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "N"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000663030',
+          E_ACTP_FLAG: '3',
+          E_ACCOUNT_BALANCE: '0.00',
+          E_AVAILABLEAMOUNT: '0.00',
+          E_CARD_NO: '62176022460000054',
+          E_ISSUE_BRANCH: '0101',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'N'
         },
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000668663",
-          E_ACTP_FLAG: "2",
-          E_ACCOUNT_BALANCE: "0.00",
-          E_AVAILABLEAMOUNT: "0.00",
-          E_CARD_NO: "62176027080005029",
-          E_ISSUE_BRANCH: "0002",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "N"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000668663',
+          E_ACTP_FLAG: '2',
+          E_ACCOUNT_BALANCE: '0.00',
+          E_AVAILABLEAMOUNT: '0.00',
+          E_CARD_NO: '62176027080005029',
+          E_ISSUE_BRANCH: '0002',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'N'
         },
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000667943",
-          E_ACTP_FLAG: "2",
-          E_ACCOUNT_BALANCE: "7962457.49",
-          E_AVAILABLEAMOUNT: "7952457.48",
-          E_CARD_NO: "62176056080010283",
-          E_ISSUE_BRANCH: "1401",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "N"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000667943',
+          E_ACTP_FLAG: '2',
+          E_ACCOUNT_BALANCE: '7962457.49',
+          E_AVAILABLEAMOUNT: '7952457.48',
+          E_CARD_NO: '62176056080010283',
+          E_ISSUE_BRANCH: '1401',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'N'
         },
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000650526",
-          E_ACTP_FLAG: "1",
-          E_ACCOUNT_BALANCE: "79355397.07",
-          E_AVAILABLEAMOUNT: "75593297.07",
-          E_CARD_NO: "62176022030539284",
-          E_ISSUE_BRANCH: "0101",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "Y"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000650526',
+          E_ACTP_FLAG: '1',
+          E_ACCOUNT_BALANCE: '79355397.07',
+          E_AVAILABLEAMOUNT: '75593297.07',
+          E_CARD_NO: '62176022030539284',
+          E_ISSUE_BRANCH: '0101',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'Y'
         },
         {
-          E_ACCOUNT_TYPE: "0004",
-          E_ACCT_NO: "50000000000000667357",
-          E_ACTP_FLAG: "2",
-          E_ACCOUNT_BALANCE: "30.00",
-          E_AVAILABLEAMOUNT: "30.00",
-          E_CARD_NO: "62176056080010085",
-          E_ISSUE_BRANCH: "1401",
-          E_CARD_STATE: "01",
-          E_ID_VERFIED: "N"
+          E_ACCOUNT_TYPE: '0004',
+          E_ACCT_NO: '50000000000000667357',
+          E_ACTP_FLAG: '2',
+          E_ACCOUNT_BALANCE: '30.00',
+          E_AVAILABLEAMOUNT: '30.00',
+          E_CARD_NO: '62176056080010085',
+          E_ISSUE_BRANCH: '1401',
+          E_CARD_STATE: '01',
+          E_ID_VERFIED: 'N'
         }
       ].map(item => ({
         cardType: item.E_ACTP_FLAG,
@@ -354,15 +353,15 @@ export default {
   },
   mounted() {
     const cardCell = new Vue(CardCell);
-    cardCell.accountTitle = "我的账户";
+    cardCell.accountTitle = '我的账户';
     cardCell.list = this.cardList;
-    cardCell.$on("selected", e => {
-      typeof e.callback === "function" && e.callback();
+    cardCell.$on('selected', e => {
+      typeof e.callback === 'function' && e.callback();
     });
-    cardCell.$on("cancel", () => {
+    cardCell.$on('cancel', () => {
       //
     });
-    cardCell.$mount("#card1");
+    cardCell.$mount('#card1');
   },
   methods: {
     eventHandler(e) {
@@ -375,41 +374,41 @@ export default {
       console.log(args);
     },
     handleSelect(item) {
-     console.log(item);
+      console.log(item);
     },
     showFullDatePicker() {
       if (!this.fullDatePicker) {
         this.fullDatePicker = new picker.DtPicker({
-          type: "date",
+          type: 'date',
           canBePermanent: true
         });
       }
       this.fullDatePicker.show(selected => {
-        alert(JSON.stringify({ text: selected.text, value: selected.value }));
+        alert(JSON.stringify({text: selected.text, value: selected.value}));
         return true;
       });
     },
     showMonthDatePicker() {
       if (!this.monthDatePicker) {
         this.monthDatePicker = new picker.DtPicker({
-          type: "month",
+          type: 'month',
           canBePermanent: true
         });
       }
       this.monthDatePicker.show(selected => {
-        alert(JSON.stringify({ text: selected.text, value: selected.value }));
+        alert(JSON.stringify({text: selected.text, value: selected.value}));
         return true;
       });
     },
     showDatePicker() {
       if (!this.datePicker) {
         this.datePicker = new picker.DtPicker({
-          type: "date",
+          type: 'date',
           canBePermanent: true
         });
       }
       this.datePicker.show(selected => {
-        alert(JSON.stringify({ text: selected.text, value: selected.value }));
+        alert(JSON.stringify({text: selected.text, value: selected.value}));
         return true;
       });
     },
@@ -418,28 +417,28 @@ export default {
         this.picker = new picker.PopPicker({
           layer: 2,
           isolated: true,
-          title: "我是picker"
+          title: '我是picker'
         });
         this.picker.setData([
           [
-            { text: "包商银行", value: 1 },
-            { text: "招商银行西二旗支行", value: 2 },
-            { text: "中国工商银行包头东河区铁西支行", value: 3 },
-            { text: "建设银行", value: 4 },
-            { text: "中国农业银行", value: 5 },
-            { text: "交通银行", value: 6 },
-            { text: "包头市农行银行", value: 7 },
-            { text: "中国银行", value: 8 }
+            {text: '包商银行', value: 1},
+            {text: '招商银行西二旗支行', value: 2},
+            {text: '中国工商银行包头东河区铁西支行', value: 3},
+            {text: '建设银行', value: 4},
+            {text: '中国农业银行', value: 5},
+            {text: '交通银行', value: 6},
+            {text: '包头市农行银行', value: 7},
+            {text: '中国银行', value: 8}
           ],
           [
-            { text: "包商银行", value: 1 },
-            { text: "招商银行西二旗支行", value: 2 },
-            { text: "中国工商银行包头东河区铁西支行", value: 3 },
-            { text: "建设银行", value: 4 },
-            { text: "中国农业银行", value: 5 },
-            { text: "交通银行", value: 6 },
-            { text: "包头市农行银行", value: 7 },
-            { text: "中国银行", value: 8 }
+            {text: '包商银行', value: 1},
+            {text: '招商银行西二旗支行', value: 2},
+            {text: '中国工商银行包头东河区铁西支行', value: 3},
+            {text: '建设银行', value: 4},
+            {text: '中国农业银行', value: 5},
+            {text: '交通银行', value: 6},
+            {text: '包头市农行银行', value: 7},
+            {text: '中国银行', value: 8}
           ]
         ]);
       }
@@ -456,64 +455,64 @@ export default {
         this.cascadePicker.setData([
           {
             value: 1,
-            text: "包商银行",
+            text: '包商银行',
             children: [
               {
                 value: 11,
-                text: "北京",
+                text: '北京',
                 children: [
-                  { value: 111, text: "小营路支行" },
-                  { value: 112, text: "大屯路支行" }
+                  {value: 111, text: '小营路支行'},
+                  {value: 112, text: '大屯路支行'}
                 ]
               },
               {
                 value: 12,
-                text: "包头",
+                text: '包头',
                 children: [
-                  { value: 121, text: "东河区西阁外支行" },
-                  { value: 122, text: "青山区幸福路支行" },
-                  { value: 123, text: "昆都仑区友谊大街支行" }
+                  {value: 121, text: '东河区西阁外支行'},
+                  {value: 122, text: '青山区幸福路支行'},
+                  {value: 123, text: '昆都仑区友谊大街支行'}
                 ]
               },
               {
                 value: 13,
-                text: "呼和浩特",
+                text: '呼和浩特',
                 children: [
-                  { value: 131, text: "赛罕区支行" },
-                  { value: 132, text: "回民区支行" },
-                  { value: 133, text: "新城区支行" }
+                  {value: 131, text: '赛罕区支行'},
+                  {value: 132, text: '回民区支行'},
+                  {value: 133, text: '新城区支行'}
                 ]
               }
             ]
           },
           {
             value: 2,
-            text: "中国工商银行-ICBC-95555",
+            text: '中国工商银行-ICBC-95555',
             children: [
               {
                 value: 21,
-                text: "上海",
+                text: '上海',
                 children: [
-                  { value: 211, text: "浦东支行" },
-                  { value: 212, text: "浦西支行" }
+                  {value: 211, text: '浦东支行'},
+                  {value: 212, text: '浦西支行'}
                 ]
               },
               {
                 value: 22,
-                text: "北京",
+                text: '北京',
                 children: [
-                  { value: 221, text: "北京西二旗支行" },
-                  { value: 222, text: "北京回龙观支行" },
-                  { value: 223, text: "北京小营支行" }
+                  {value: 221, text: '北京西二旗支行'},
+                  {value: 222, text: '北京回龙观支行'},
+                  {value: 223, text: '北京小营支行'}
                 ]
               },
               {
                 value: 23,
-                text: "西安",
+                text: '西安',
                 children: [
-                  { value: 231, text: "碑林区西大街支行" },
-                  { value: 232, text: "雁塔区西安电子科技大学支行" },
-                  { value: 233, text: "长安区郭杜镇支行" }
+                  {value: 231, text: '碑林区西大街支行'},
+                  {value: 232, text: '雁塔区西安电子科技大学支行'},
+                  {value: 233, text: '长安区郭杜镇支行'}
                 ]
               }
             ]
@@ -523,14 +522,14 @@ export default {
       this.cascadePicker.show(selected => {
         alert(
           `选择的数据： ${JSON.stringify(
-            selected.map(item => ({ text: item.text, value: item.value }))
+            selected.map(item => ({text: item.text, value: item.value}))
           )}`
         );
         return true;
       });
     },
     confirmDialog() {
-      alert("点了确认升级");
+      alert('点了确认升级');
     },
     showCashier() {
       this.cashier = cashier.init();
